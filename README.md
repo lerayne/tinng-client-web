d5aa9ad1da3825b94a5fbc22c5189d6d88b88855
Michael Yegorov <lerayne@gmail.com>
2014-10-21 21:57:34 +0300

    init

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ tinng-client-web
 ================
 
 Web client for tinng service
+
+##Development
+
+Dont forget to do ``bower update`` (bower dependencies do not stored in repo!)
---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Lerayne on 21.10.2014.
+ */
+
+$(function(){
+
+	// in Polymer you now can create new actibe elements as easy as this:
+	$('body').append($('<page-main></page-main>'));
+
+})
