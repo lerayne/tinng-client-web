c5f88c3dcbc24fc01a9c232bfbc2c0ec1a8fc98e
Michael Yegorov <lerayne@gmail.com>
2014-12-15 10:48:13 +0200

    some unmerged changes

--- scripts/main.js
+++ scripts/main.js
@@ -7,25 +7,17 @@ tinng = {
 
 
 
-
-
 $(function(){
 
-	//функцию плагина разместите внутри коллбэка о готовности DOM
-	/*outdatedBrowser({
-		bgColor: '#f25648',
-		color: '#ffffff',
-		lowerThan: 'IE10',
-		languagePath: 'bower_components/outdated-browser/outdatedbrowser/lang/ru.html'
-	});*/
-
+	// If this is not IE or its IE 10 - launch the webapp (IE11 and greater aren't having "MSIE" in userAgent)
 	if (navigator.userAgent.indexOf('MSIE') == -1 || navigator.userAgent.indexOf('MSIE 10') != -1) {
 
-		// in Polymer you now can create new actibe elements as easy as this:
+		// in Polymer you now can create new active elements as easy as this:
 		$('#app-frame').append($('<page-main>'));
 
 	} else {
 
+		// get excuse update-your-browser redirect
 		$.get('includes/outdated.html').then(function(html){
 			$('#app-frame').append(html);
 		})
