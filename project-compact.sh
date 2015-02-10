d778bdb1e11bd4c840d12280001d48e1bc4110c1
Michael Yegorov <lerayne@gmail.com>
2015-02-10 11:37:03 +0200

    Vulcanization fix

--- project-compact.sh
+++ project-compact.sh
@@ -2,3 +2,5 @@
 
 vulcanize index-source.html -o index.html -s --config vulcanize-config.json
 vulcanize index-source.html -o index-dev.html --config vulcanize-config.json
+vulcanize skins/paper/skin-paper.html -s -o skins/paper/skin-paper-v.html
+vulcanize skins/paper/skin-paper.html -o skins/paper/skin-paper-dv.html
