79b00e9a19d8519fbd99264e077cd232311ca7d4
Michael Yegorov <lerayne@gmail.com>
2015-06-19 14:27:37 +0300

    vulcanize change

--- project-compact.sh
+++ project-compact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-vulcanize index-source.html -o index.html -s --config vulcanize-config.json
-vulcanize index-source.html -o index-dev.html --config vulcanize-config.json
+vulcanize index-source.html > index.html -s --config vulcanize-config.json
+vulcanize index-source.html > index-dev.html --config vulcanize-config.json
 vulcanize skins/paper/skin-paper.html -s -o skins/paper/skin-paper-v.html
 vulcanize skins/paper/skin-paper.html -o skins/paper/skin-paper-dv.html
