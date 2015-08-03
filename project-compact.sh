ec1d279080db0b2e9797d7ff89584450cbf1877a
Michael Yegorov <lerayne@gmail.com>
2015-08-04 01:52:25 +0300

    vulcanize options

--- project-compact.sh
+++ project-compact.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-vulcanize index-source.html > index.html
-vulcanize skins/paper/skin-paper.html > skins/paper/skin-paper-v.html
+vulcanize index-source.html -o index.html
+vulcanize skins/paper/skin-paper.html -o skins/paper/skin-paper-v.html
