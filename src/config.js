812941f7d92c316d0be6357788e076496d80df96
Michael Yegorov <lerayne@gmail.com>
2016-09-29 13:26:58 +0300

    moved config to a global variable 2

---
+++ src/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by M. Yegorov on 2016-09-29.
+ */
+
+globalConfig = {
+    pollIntervalActive: 5000,
+    pollIntervalPassive: 60000,
+    serverURL: "http://localhost:8888/tinng-server-php"
+};
